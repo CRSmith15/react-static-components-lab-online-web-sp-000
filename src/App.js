d98d179cc7718a83535cae438d5aa600d2d2caff
Chance Smith <chancesmith97@hotmail.com>
2020-10-01 23:13:51 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends Component {
 		// your code in the return statement below!
 		return (
 			<div className="App">
-				<CatComponent />
-				<GraceHopperQuoteComponent />
+				
 				{/* one more component missing */}
 			</div>
 		);
